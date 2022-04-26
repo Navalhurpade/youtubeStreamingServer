f0bf478b2d8ec6fc832130b30ea2ed9d98fb507b
Naval Hurpade <naval@sorigin.com>
2022-04-26 11:53:30 +0530

    Duration issue fixed.

--- app.js
+++ app.js
@@ -11,42 +11,44 @@ app.get('/', async (req, res) => {
   let url = 'https://www.youtube.com/watch?v=0cbXeXP2Wgg';
   let data = await ytdl.getInfo(url);
   let audioFormat = ytdl.filterFormats(data.formats, 'audioonly');
-  
+
   let videoFormat = ytdl.filterFormats(data.formats, 'videoonly');
-  
-  let selectedAudioFormat = audioFormat
-    .sort((formatA, formatB) => formatB.audioBitrate - formatA.audioBitrate)[0];
-  
-  let selectedVideoFormat = videoFormat
-    .sort((formatA, formatB) => {
-      if (formatB.height === formatA.height) {
-        return formatB.fps - formatA.fps;
-      }
-      return formatB.height - formatA.height;
-    })[0];
-  
+
+  let selectedAudioFormat = audioFormat.sort((formatA, formatB) => formatB.audioBitrate - formatA.audioBitrate)[0];
+
+  let selectedVideoFormat = videoFormat.sort((formatA, formatB) => {
+    if (formatB.height === formatA.height) {
+      if (formatB.container === 'mp4') return 1;
+      return formatB.fps - formatA.fps;
+    }
+    return formatB.height - formatA.height;
+  })[0];
+
   try {
     // res.send(data)
     let audio = ytdl.downloadFromInfo(data, {
       format: selectedAudioFormat,
-      quality: "highestaudio"
+      quality: 'highestaudio',
     });
-    
+
     let video = ytdl.downloadFromInfo(data, {
       quality: 'highestvideo',
-      format: selectedVideoFormat
+      format: selectedVideoFormat,
     });
+
     // res.send({selectedAudioFormat, selectedVideoFormat});
-    converterProcess(audio, video, selectedAudioFormat, selectedVideoFormat, res)
-    
-    res.setHeader(
-      'Content-Disposition',
-      'attachment;filename=' +
-      'sas.' +
-      selectedVideoFormat.container
-    );
+    converterProcess(audio, video, selectedAudioFormat, selectedVideoFormat, res);
+
+    // FfmpegCommand()
+    //  .input(video)
+    //  .input(audio)
+    //  .mergeToFile(`file.${selectedVideoFormat.container}`)
+    //  .pipe(res);
+    //
+
+    res.setHeader('Content-Disposition', 'attachment;filename=' + 'sas.' + selectedVideoFormat.container);
     res.setHeader('Content-Type', 'video/mp4');
-    
+
     // res.send(data.formats.filter((q) => q.qualityLabel === '2160p' && q.container === 'mp4')[0]);
   } catch (e) {
     console.log(e);
--- ffmpegProcess.js
+++ ffmpegProcess.js
@@ -1,17 +1,16 @@
 const ffmpegInstallation = require('@ffmpeg-installer/ffmpeg');
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 const fs = require('fs');
-
 
 function millisToMinutesAndSeconds(millis) {
   let minutes = Math.floor(millis / 60000);
-  let seconds = ((millis % 60000) / 1000).toFixed(0);
-  return `${minutes}:${seconds < 10 ? 0 : ''}${seconds}`;
+  let seconds = Math.round(millis / 1000);
+  // return `${minutes}m:${seconds < 10 ? 0 : ''}${seconds}s`;
+  return `${seconds}s`;
 }
 
-
 module.exports = function (audio, video, selectedAudioFormat, selectedVideoFormat, res) {
-  console.log("FORMATS :: ", selectedAudioFormat,selectedVideoFormat);
+  console.log('FORMATS :: ', selectedAudioFormat, selectedVideoFormat);
   const ffmpegProcess = spawn(
     ffmpegInstallation.path,
     [
@@ -41,39 +40,38 @@ module.exports = function (audio, video, selectedAudioFormat, selectedVideoForma
       selectedVideoFormat.container,
       '-loglevel',
       'debug',
+      '-t',
+      `${millisToMinutesAndSeconds(selectedVideoFormat.approxDurationMs)}`,
       '-strict',
       '-2',
       '-',
-      // `${Math.random()}.${selectedVideoFormat.container}`
     ],
     {
-      stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe']
+      stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe'],
     }
   );
-  console.log("CODECS ::", selectedVideoFormat.videoCodec, selectedAudioFormat.audioCodec);
-  
-  let file = fs.createWriteStream('./video.mp4')
-  
+  console.log('CODECS ::', selectedVideoFormat.videoCodec, selectedAudioFormat.audioCodec);
+
+  let file = fs.createWriteStream('./video.mp4');
+
   video.pipe(ffmpegProcess.stdio[3]);
   audio.pipe(ffmpegProcess.stdio[4]);
   ffmpegProcess.stdio[1].pipe(res);
-  
+
   let ffmpegLogs = '';
-  
+
   ffmpegProcess.stdio[2].on('data', (chunk) => {
     ffmpegLogs += chunk.toString();
-    console.log("REFLOGS :: ",ffmpegLogs);
-  
+    console.log('REFLOGS :: ', ffmpegLogs);
   });
-  
+
   ffmpegProcess.on('exit', (exitCode) => {
     if (exitCode === 1) {
       console.error('ERROR IN CHILD ::', ffmpegLogs);
     }
-    console.log("Exit Child ::",exitCode);
+    console.log('Exit Child ::', exitCode);
   });
   ffmpegProcess.on('error', (err) => {
     console.log('ERROR in CHILD ERROR :: ', err);
   });
 };
-
